413bea67835cf86fa8e539da24548595389a3255
Jay Moorthi <jay@tddium.com>
2011-06-19 21:12:01 -0700

    Rails app skeleton without activerecord.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidSample::Application.config.secret_token = '076c64e99dcfa10baebe1d3928e143ce92094c0087189eb7830bfd6b3c87e483518f22d286a1e2010d325d8609188037081af84c1d510f695c1df052d4cc89c1'
