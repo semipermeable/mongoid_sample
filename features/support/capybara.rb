4eb3ec0ca4bfdfe004014e87ca3b254ff047e17b
Jay Moorthi <jay@tddium.com>
2011-08-26 15:50:51 -0700

    set the server port

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -4,6 +4,7 @@ end
 
 if ENV['TDDIUM']
   Capybara.javascript_driver = :tddium
+  Capybara.server_port = 12000 + (ENV['TDDIUM_TEST_EXEC_ID'].to_i % 4)
 else
   Capybara.javascript_driver = :selenium
 end
