5c2d29c3a161a12a9c1c6f3c5b45a85c1c0949b5
Jay Moorthi <jay@tddium.com>
2011-08-26 15:43:37 -0700

    different structure

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,7 +1,5 @@
-require 'capybara'
-
 Capybara.register_driver :tddium do |app|
-   Capybara::Selenium::Driver.new(app, :browser => :remote)
+   Capybara::Driver::Selenium.new(app, :browser => :remote)
 end
 
 if ENV['TDDIUM']
