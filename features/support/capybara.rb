3ee6b7bfeae412e8232dc0fbf0b6a9eb13be33ed
Jay Moorthi <jay@tddium.com>
2011-09-12 15:07:32 -0700

    use better port finding logic

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,1 +1,11 @@
 Capybara.javascript_driver = :selenium
+def find_available_port
+    server = TCPServer.new('127.0.0.1', 0)
+      server.addr[1]
+ensure
+    server.close if server
+end
+
+if ENV['TDDIUM'] then
+    Capybara.server_port = find_available_port
+end
